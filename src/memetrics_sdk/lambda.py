72ff16e3dd32d319b455adacc977bcdb64d06387
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2023-08-10 12:24:25 -0300

    Add memetrics lambda example request

---
+++ src/memetrics_sdk/lambda.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+import requests
+
+url = "https://us-east-1.aws.data.mongodb-api.com/app/data-eimra/endpoint/data/v1/action/insertOne"
+
+document = {"event": ...}
+
+payload = json.dumps(
+    {
+        "collection": "searchresult",
+        "database": "datasources-osf",
+        "dataSource": "wingman-prod",
+        "document": document,
+    }
+)
+headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Request-Headers": "*",
+    "api-key": os.environ["MONGODB_API_KEY"],
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
