89df1ec941b10cf192cd28b0dcdf26ad42963bc8
Jônatas Wehrmann <jonataswehrmann@gmail.com>
2023-09-13 15:11:19 -0300

    Add eggregator webclient

--- py-sdk/memetrics_sdk/schemas.py
+++ py-sdk/memetrics_sdk/schemas.py
@@ -6,3 +6,30 @@ TAuthHeaders = TypedDict(
     {"Authorization": str, "X-User-Email": str, "X-User-ID": str, "X-User-IP": str},
     total=False,
 )
+
+
+EventsPerUSer = TypedDict(
+    "EventsPerUser",
+    {
+        "_id": str,
+        "action": str,
+        "app": str,
+        "type": str,
+        "user_email": str,
+        "count": int,
+        "date": str,
+        "events": list,
+    },
+)
+
+EventData = TypedDict(
+    "EventData",
+    {
+        "action": str,
+        "app": str,
+        "app_version": str,
+        "extra": list,
+        "type": str,
+        "user": dict,
+    },
+)
--- py-sdk/memetrics_sdk/webservice.py
+++ py-sdk/memetrics_sdk/webservice.py
@@ -1,12 +1,13 @@
 import os
 import json
+from datetime import datetime
 
 from typing import Optional, cast
 
 import httpx
-from memetrics.events.schemas import EventData, GeneratedFields
+from memetrics.events.schemas import GeneratedFields
 
-from .schemas import TAuthHeaders
+from .schemas import TAuthHeaders, EventData
 
 
 class WebserviceClient:
@@ -54,3 +55,60 @@ class WebserviceClient:
 
     def __del__(self):
         self.http_client.close()
+
+
+class EggregatorClient:
+    def __init__(self):
+        self.api_key = os.environ["TEIA_API_KEY"]
+        self.url = os.environ["MEMETRICS_URL"]
+        self.headers = {
+            "Authorization": f"Bearer {self.api_key}",
+        }
+        self.http_client = httpx.Client(
+            base_url=self.url,
+            headers=self.headers,
+        )
+
+    def read_count_by_user(
+        self,
+        user_email: list[str],
+        action: str = None, app: str = None,
+        date_gte: datetime = None, date_lt: datetime = None,
+        groupby: str = "day", limit: int = 10, offset: int = 0,
+        sort: str = "-count", type: str = None,
+        headers: Optional[TAuthHeaders] = None
+    ) -> EventData:
+
+        relative_url = "/eggregations/count-by-user"
+
+        params = {
+            "user_email": user_email,
+        }
+
+        if action:
+            params["action"] = action
+        if app:
+            params["app"] = app
+        if date_gte:
+            params["date:gte"] = date_gte
+        if date_lt:
+            params["date:lt"] = date_lt
+        if type:
+            params["type"] = type
+
+        if groupby:
+            params["$groupby"] = groupby
+        if limit:
+            params["$limit"] = limit
+        if offset:
+            params["$offset"] = offset
+        if sort:
+            params["$sort"] = sort
+
+        response = self.http_client.get(
+            relative_url,
+            headers=cast(dict[str, str], headers),
+            params=params,
+        )
+        response.raise_for_status()
+        return response.json()
