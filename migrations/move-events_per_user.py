247eb118ee6abc4de9de3bfbd86a6b57f7de1903
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2023-09-20 19:19:08 -0300

    Fix migration bug: missing events per user insertion

--- migrations/move-events_per_user.py
+++ migrations/move-events_per_user.py
@@ -8,7 +8,7 @@ from memetrics.events.models import EventsPerUser
 from memetrics.events.schemas import Event
 
 dotenv.load_dotenv()
-BATCH_SIZE = 512
+BATCH_SIZE = 1024
 client = MongoClient(os.environ["MEME_MONGODB_URI"])
 db = client[os.environ["MEME_MONGODB_DBNAME"]]
 db_local = MongoClient()["egg"]
@@ -18,10 +18,9 @@ db["events_per_user"].delete_many(filters)
 count = db_local["events_per_user"].count_documents(filters)
 for obj in tqdm.tqdm(db_local["events_per_user"].find(filters).batch_size(BATCH_SIZE), total=count):
     obj_batch.append(obj)
-    tqdm.tqdm.write(obj["user_email"])
     if len(obj_batch) == BATCH_SIZE:
-        tqdm.tqdm.write(repr(db["events_per_user"].insert_many(obj_batch)))
+        db["events_per_user"].insert_many(obj_batch)
         obj_batch = []
 if obj_batch:
-    tqdm.tqdm.write(repr(db["events_per_user"].insert_many(obj_batch)))
+    db["events_per_user"].insert_many(obj_batch)
     obj_batch = []
