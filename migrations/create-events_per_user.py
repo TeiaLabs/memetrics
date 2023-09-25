ecc6de4b445f9e4ef5fb600cc4a55038b7b7917b
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2023-09-25 00:42:57 -0300

    Extract batching logic in migration scripts

--- migrations/create-events_per_user.py
+++ migrations/create-events_per_user.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from typing import Iterable, TypeVar
 
 import dotenv
 import tqdm
@@ -8,22 +9,34 @@ from pymongo import MongoClient
 from memetrics.events.models import EventsPerUser
 from memetrics.events.schemas import Event
 
+T = TypeVar("T")
+
+
+def batchify_iter(
+    it: Iterable[T],
+    batch_size: int,
+) -> Iterable[list[T]]:
+    batch = []
+    for item in it:
+        batch.append(item)
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+    if batch:
+        yield batch
+
+
 dotenv.load_dotenv()
 BATCH_SIZE = 4096
 client = MongoClient(os.environ["MEME_MONGODB_URI"])
 db = client[os.environ["MEME_MONGODB_DBNAME"]]
-db_local = MongoClient()["egg"]
-obj_batch = []
-filters = {"created_at": {"$gte": datetime.fromisoformat("2024-07-01")}}
+db_local = MongoClient()["nei"]
+filters = {"created_at": {"$gte": datetime.fromisoformat("2024-01-01")}}
+
 count = db["events"].count_documents(filters)
-for obj in tqdm.tqdm(db["events"].find(filters).batch_size(BATCH_SIZE), total=count):
-    obj_batch.append(obj)
-    if len(obj_batch) == BATCH_SIZE:
-        obj_batch = [Event(**obj) for obj in obj_batch]
-        EventsPerUser.bulk_increment_from_events(obj_batch, db_local)
-        obj_batch = []
-if obj_batch:
+cursor = db["events"].find(filters)
+batched_cursor = batchify_iter(cursor, BATCH_SIZE)
+for obj_batch in tqdm.tqdm(batched_cursor, total=count // BATCH_SIZE + 1):
     obj_batch = [Event(**obj) for obj in obj_batch]
     EventsPerUser.bulk_increment_from_events(obj_batch, db_local)
-    obj_batch = []
 client.close()
--- migrations/move-events_per_user.py
+++ migrations/move-events_per_user.py
@@ -1,26 +1,38 @@
 import os
+from typing import Iterable, TypeVar
 
 import dotenv
 import tqdm
 from pymongo import MongoClient
 
-from memetrics.events.models import EventsPerUser
-from memetrics.events.schemas import Event
+T = TypeVar("T")
+
+
+def batchify_iter(
+    it: Iterable[T],
+    batch_size: int,
+) -> Iterable[list[T]]:
+    batch = []
+    for item in it:
+        batch.append(item)
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+    if batch:
+        yield batch
+
 
 dotenv.load_dotenv()
 BATCH_SIZE = 1024
 client = MongoClient(os.environ["MEME_MONGODB_URI"])
 db = client[os.environ["MEME_MONGODB_DBNAME"]]
 db_local = MongoClient()["egg"]
-obj_batch = []
 filters = {"date": {"$gte": "2024-01-01"}}
 db["events_per_user"].delete_many(filters)
+
 count = db_local["events_per_user"].count_documents(filters)
-for obj in tqdm.tqdm(db_local["events_per_user"].find(filters).batch_size(BATCH_SIZE), total=count):
-    obj_batch.append(obj)
-    if len(obj_batch) == BATCH_SIZE:
-        db["events_per_user"].insert_many(obj_batch)
-        obj_batch = []
-if obj_batch:
+cursor = db_local["events_per_user"].find(filters)
+for obj_batch in tqdm.tqdm(batchify_iter(cursor, BATCH_SIZE), total=count // BATCH_SIZE + 1):
     db["events_per_user"].insert_many(obj_batch)
-    obj_batch = []
+
+client.close()
