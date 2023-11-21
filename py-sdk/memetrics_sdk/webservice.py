9325a52c65d89cc944c2176ef7c3afa4188222ec
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2023-11-21 14:18:06 -0300

    Safely error out on missing env vars

--- py-sdk/memetrics_sdk/webservice.py
+++ py-sdk/memetrics_sdk/webservice.py
@@ -6,14 +6,15 @@ from typing import Optional, cast
 import httpx
 from memetrics.events.schemas import GeneratedFields
 
+from .errors import MissingAPIKeyError
 from .schemas import EventData, TAuthHeaders
 
 
 class WebserviceClient:
     def __init__(
         self,
-        url: str = os.environ.get("MEMETRICS_URL"),
-        api_key: str = os.environ.get("TEIA_API_KEY"),
+        api_key: Optional[str] = os.getenv("TEIA_API_KEY", None),
+        url: Optional[str] = os.getenv("MEMETRICS_URL", "https://memetrics.teialabs.com.br"),
     ):
         if url is None:
             raise ValueError("URL not defined.")
@@ -23,6 +24,9 @@ class WebserviceClient:
 
         self.api_key = api_key
         self.url = url
+        if self.api_key is None:
+            m = "'TEIA_API_KEY' env var is required or api_key param must be passed."
+            raise MissingAPIKeyError(m)
         self.headers = {
             "Authorization": f"Bearer {self.api_key}",
         }
