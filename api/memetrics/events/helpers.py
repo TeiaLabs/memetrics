1cfe726c90a4b5d45bf3745393cfdfda49b134a5
severo <joaovbsevero@gmail.com>
2025-04-30 13:54:37 -0300

    Fix user extra overriding

--- api/memetrics/events/helpers.py
+++ api/memetrics/events/helpers.py
@@ -1,16 +1,20 @@
 from tauth.schemas import Creator
 
-from .schemas import EventData, Attribute
+from .schemas import Attribute, EventData
 
 
 def help_user_edge_cases(body: EventData, creator: Creator) -> EventData:
     if not body.user.get("email"):
         body.user["email"] = creator.user_email
-        body.user["extra"] = [
-            Attribute(
-                name="ip_address",
-                type="string",
-                value=creator.user_ip,
-            )
-        ]
+
+    if not body.user["extra"]:
+        body.user["extra"] = []
+
+    body.user["extra"].append(
+        Attribute(
+            name="ip_address",
+            type="string",
+            value=creator.user_ip,
+        )
+    )
     return body
